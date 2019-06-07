6b601546de6329a494be9dc09b71384a948b8a2f
rpavlov <roumen@rpavlov.com>
2019-06-07 17:14:02 +0200

    initial commit

--- set1.py
+++ set1.py
@@ -1,7 +1,15 @@
+import base64
+
+# Convert hex to base64
 def convert64(hex_value):
-    print("yolo")
+    return base64.b64encode(bytes.fromhex(hex_value))
 
-def main():
-    convert64(123)
+# Fixed XOR
+def xor():
+    str1=b'1c0111001f010100061a024b53535009181c'
+    str2=b'686974207468652062756c6c277320657965'
+    int1=int(str1.encode().hex(), 16)
+    int2=int(str2.encode().hex(), 16)
 
-main()
+    print(hex(int1 ^ int2))
+
