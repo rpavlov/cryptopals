658d3a7c42557514ddbef3781daccbc17ccb89e5
rpavlov <roumen@rpavlov.com>
2019-06-11 10:26:24 +0200

    Complete 1.2

--- set1.py
+++ set1.py
@@ -5,11 +5,9 @@ def convert64(hex_value):
     return base64.b64encode(bytes.fromhex(hex_value))
 
 # Fixed XOR
-def xor():
-    str1=b'1c0111001f010100061a024b53535009181c'
-    str2=b'686974207468652062756c6c277320657965'
-    int1=int(str1.encode().hex(), 16)
-    int2=int(str2.encode().hex(), 16)
-
-    print(hex(int1 ^ int2))
+def xor(str1='1c0111001f010100061a024b53535009181c',
+        str2='686974207468652062756c6c277320657965'):
+    int1=int(str1, 16)
+    int2=int(str2, 16)
+    return hex(int1 ^ int2)[2:]
 
